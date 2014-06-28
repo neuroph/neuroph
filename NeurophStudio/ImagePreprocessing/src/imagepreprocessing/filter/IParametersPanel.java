6eef80a1b8e01941c80a540a699f05979d243265
sevarac <sevarac@4ad91d96-3078-4544-b664-688ae3f1b027>
2014-06-28 14:30:40 +0000

    image preprocessig fixing

---
+++ NeurophStudio/ImagePreprocessing/src/imagepreprocessing/filter/IParametersPanel.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package imagepreprocessing.filter;
+
+import javax.swing.JPanel;
+
+
+
+/**
+ *
+ * Interface for filters that have parameters
+ * 
+ * @author Sanja
+ */
+public interface IParametersPanel {
+    public JPanel getPanel();
+}
+//Aleksandar
---
+++ NeurophStudio/ImagePreprocessing/src/imagepreprocessing/filter/ImageFilterChain.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imagepreprocessing.filter;
+
+import imagepreprocessing.helper.FilteredImage;
+import java.awt.image.BufferedImage;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Process images by applying all filters in chain 
+ * @author Sanja
+ */
+public class ImageFilterChain implements ImageFilter, Serializable {
+
+    private List<ImageFilter> filters = new ArrayList<ImageFilter>();
+    private String chainName;
+    /**
+     * Add filter to chain
+     * @param filter filter to be added
+     */
+    public void addFilter(ImageFilter filter){
+        filters.add(filter);
+    }
+    /**
+     * Remove filter from chain
+     * @param filter filter to be removed
+     * @return true if filter is removed 
+     */
+    public boolean removeFilter(ImageFilter filter){
+        return filters.remove(filter);
+    }
+
+    /**
+     * Apply all filters from a chain on image 
+     * @param image image to process
+     * @return processed image
+     */
+    @Override
+    public BufferedImage processImage(BufferedImage image) {
+        
+        BufferedImage tempImage = image;
+        for (ImageFilter filter : filters) {
+            BufferedImage filteredImage = filter.processImage(tempImage);
+            tempImage = filteredImage;
+        }
+
+        return tempImage;
+
+    }
+    /**
+     * Returns images of all stages in processing
+     * Used for testing 
+     * @param image
+     * @return 
+     */
+    public List<FilteredImage> processImageTest(BufferedImage image){
+        List<FilteredImage> list = new ArrayList<FilteredImage>();
+        BufferedImage tempImage = image;
+        for (ImageFilter filter : filters) {
+            BufferedImage processedImage = filter.processImage(tempImage);
+            String filterName = filter.toString();
+            FilteredImage filteredImage = new FilteredImage(processedImage,filterName);
+            list.add(filteredImage);
+            tempImage = processedImage;
+        }
+
+        return list;
+    }
+    /**
+     * Get filters from chain
+     * @return 
+     */
+    public List<ImageFilter> getFilters(){
+        return filters;
+    }
+
+    public void setFilters(List<ImageFilter> filters) {
+        this.filters = filters;
+    }
+
+    public String getChainName() {
+        return chainName;
+    }
+
+    public void setChainName(String chainName) {
+        this.chainName = chainName;
+    }
+
+    @Override
+    public String toString() {
+        return chainName;
+    }
+    
+}
