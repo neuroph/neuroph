1493d4160813d4e20c10688ade3fd6a0d51e5693
sevarac <sevarac@4ad91d96-3078-4544-b664-688ae3f1b027>
2014-06-28 14:31:27 +0000

    image preprocessig fixing

---
+++ NeurophStudio/ImagePreprocessing/src/imagepreprocessing/helper/PreprocessingHelper.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imagepreprocessing.helper;
+
+/**
+ * Class that contains all filter independent methods
+ * that are being used in image filters implementation
+ * @author Sanja
+ */
+public class PreprocessingHelper {
+
+    public static int colorToRGB(int alpha, int red, int green, int blue) {
+
+        int newPixel = 0;
+        newPixel += alpha;
+        newPixel = newPixel << 8;
+        newPixel += red;
+        newPixel = newPixel << 8;
+        newPixel += green;
+        newPixel = newPixel << 8;
+        newPixel += blue;
+
+        return newPixel;
+    }
+}
