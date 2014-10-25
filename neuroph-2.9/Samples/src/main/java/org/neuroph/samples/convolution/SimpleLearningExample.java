4ea96973caa9a60fb2e34d8447da7f3f34876c04
mithquissir <bfulurija@gmail.com>
2014-10-25 22:25:38 +0200

    initial commit

--- neuroph-2.9/Samples/src/main/java/org/neuroph/samples/convolution/SimpleLearningExample.java
+++ neuroph-2.9/Samples/src/main/java/org/neuroph/samples/convolution/SimpleLearningExample.java
@@ -11,63 +11,63 @@ import org.neuroph.util.NeuralNetworkFactory;
 
 public class SimpleLearningExample {
 
-	public static void testLearningOneLayer() {
-		ConvolutionalNetwork convolutionNet = new ConvolutionalNetwork();
-		Kernel convolutionKernel = new Kernel(5, 5);
+    public static void testLearningOneLayer() {
+        ConvolutionalNetwork convolutionNet = new ConvolutionalNetwork();
+        Kernel convolutionKernel = new Kernel(5, 5);
 
-		// create input layer
-		Layer2D.Dimensions inputDimension = new Layer2D.Dimensions(5, 5);
-		InputMapsLayer inputLayer = new InputMapsLayer(inputDimension, 1);
-		// add input layer to network
-		convolutionNet.addLayer(inputLayer);
+        // create input layer
+        Layer2D.Dimensions inputDimension = new Layer2D.Dimensions(5, 5);
+        InputMapsLayer inputLayer = new InputMapsLayer(inputDimension, 1);
+        // add input layer to network
+        convolutionNet.addLayer(inputLayer);
 
-                // create convolution layer with 2 feature maps
-                ConvolutionalLayer convolutionLayer = new ConvolutionalLayer(inputLayer, convolutionKernel, 2);                                
-		// add convolution layer to network
-		convolutionNet.addLayer(convolutionLayer);
+        // create convolution layer with 2 feature maps
+        ConvolutionalLayer convolutionLayer = new ConvolutionalLayer(inputLayer, convolutionKernel, 2);
+        // add convolution layer to network
+        convolutionNet.addLayer(convolutionLayer);
 
-		// connectFeatureMaps input and convolution layer
-		ConvolutionalUtils.connectFeatureMaps(inputLayer, convolutionLayer, 0, 0);
-		ConvolutionalUtils.connectFeatureMaps(inputLayer, convolutionLayer, 0, 1);
+        // connectFeatureMaps input and convolution layer
+        ConvolutionalUtils.connectFeatureMaps(inputLayer, convolutionLayer, 0, 0);
+        ConvolutionalUtils.connectFeatureMaps(inputLayer, convolutionLayer, 0, 1);
 
-		NeuralNetworkFactory.setDefaultIO(convolutionNet);
+        NeuralNetworkFactory.setDefaultIO(convolutionNet);
 
-		// CREATE DATA SET
+        // CREATE DATA SET
 
-		DataSet dataSet = new DataSet(25, 2);
-		dataSet.addRow(new double[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-				new double[] { 1, 0 });
-		dataSet.addRow(new double[] { 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0 },
-				new double[] { 0, 1 });
+        DataSet dataSet = new DataSet(25, 2);
+        dataSet.addRow(new double[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                new double[]{1, 0});
+        dataSet.addRow(new double[]{0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0},
+                new double[]{0, 1});
 
-		// TRAIN NETWORK
+        // TRAIN NETWORK
 
-		convolutionNet.getLearningRule().setMaxError(0.00001);
-		convolutionNet.learn(dataSet);
+        convolutionNet.getLearningRule().setMaxError(0.00001);
+        convolutionNet.learn(dataSet);
 
-		WeightVisualiser visualiser1 = new WeightVisualiser(convolutionLayer.getFeatureMap(0), convolutionKernel);
-		visualiser1.displayWeights();
+        WeightVisualiser visualiser1 = new WeightVisualiser(convolutionLayer.getFeatureMap(0), convolutionKernel);
+        visualiser1.displayWeights();
 
-		WeightVisualiser visualiser2 = new WeightVisualiser(convolutionLayer.getFeatureMap(1), convolutionKernel);
-		visualiser2.displayWeights();
+        WeightVisualiser visualiser2 = new WeightVisualiser(convolutionLayer.getFeatureMap(1), convolutionKernel);
+        visualiser2.displayWeights();
 
-		// CREATE TEST SET
+        // CREATE TEST SET
 
-		DataSet testSet = new DataSet(25, 2);
-		testSet.addRow(new double[] { 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-				new double[] { 1, 0 });
-		testSet.addRow(new double[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 },
-				new double[] { 1, 0 });
-		testSet.addRow(new double[] { 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0 },
-				new double[] { 0, 1 });
+        DataSet testSet = new DataSet(25, 2);
+        testSet.addRow(new double[]{0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                new double[]{1, 0});
+        testSet.addRow(new double[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0},
+                new double[]{1, 0});
+        testSet.addRow(new double[]{0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0},
+                new double[]{0, 1});
 
-		// TEST NETWORK
+        // TEST NETWORK
 
-		ModelMetric.calculateModelMetric(convolutionNet, testSet);
+        ModelMetric.calculateModelMetric(convolutionNet, testSet);
 
-	}
+    }
 
-	public static void main(String[] args) {
-		testLearningOneLayer();
-	}
+    public static void main(String[] args) {
+        testLearningOneLayer();
+    }
 }
