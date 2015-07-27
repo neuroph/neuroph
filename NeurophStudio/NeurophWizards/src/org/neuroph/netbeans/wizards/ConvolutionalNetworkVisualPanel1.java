7fa0f793cae110497e683e9c054cadf35e1e0bd8
Stefan <Stefan@Stefi>
2015-07-27 15:34:18 +0200

    Convnets changes

--- NeurophStudio/NeurophWizards/src/org/neuroph/netbeans/wizards/ConvolutionalNetworkVisualPanel1.java
+++ NeurophStudio/NeurophWizards/src/org/neuroph/netbeans/wizards/ConvolutionalNetworkVisualPanel1.java
@@ -102,6 +102,12 @@ public class ConvolutionalNetworkVisualPanel1 extends javax.swing.JPanel {
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(12, Short.MAX_VALUE)
+                .addComponent(jLabel2)
+                .addGap(18, 18, 18)
+                .addComponent(txtNumberOfMaps, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(76, 76, 76))
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -112,20 +118,16 @@ public class ConvolutionalNetworkVisualPanel1 extends javax.swing.JPanel {
                         .addGap(15, 15, 15)
                         .addComponent(txtInputHeight, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel2)
+                        .addComponent(jLabel4)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(txtNumberOfMaps, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(txtOutputNeuronCount, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel3)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(txtKernelWidth, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(txtKernelHeight, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel4)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(txtOutputNeuronCount, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(92, Short.MAX_VALUE))
+                        .addComponent(txtKernelHeight, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -144,11 +146,11 @@ public class ConvolutionalNetworkVisualPanel1 extends javax.swing.JPanel {
                     .addComponent(jLabel3)
                     .addComponent(txtKernelWidth, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(txtKernelHeight, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
+                .addGap(58, 58, 58)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(txtNumberOfMaps, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel2))
-                .addContainerGap(129, Short.MAX_VALUE))
+                .addContainerGap(89, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
@@ -210,6 +212,7 @@ public class ConvolutionalNetworkVisualPanel1 extends javax.swing.JPanel {
         txtInputHeight.setText("");
         txtInputWidth.setText("");
         txtNumberOfMaps.setText("");
+        txtOutputNeuronCount.setText("");
     }
 
 }
--- NeurophStudio/NeurophWizards/src/org/neuroph/netbeans/wizards/spi/ConvolutionalNetworkWizard.java
+++ NeurophStudio/NeurophWizards/src/org/neuroph/netbeans/wizards/spi/ConvolutionalNetworkWizard.java
@@ -1,9 +1,14 @@
 package org.neuroph.netbeans.wizards.spi;
 
 import java.awt.Component;
+import java.util.List;
+import javax.swing.JPanel;
 import org.neuroph.core.NeuralNetwork;
 import org.neuroph.core.learning.LearningRule;
+import org.neuroph.netbeans.wizards.ConvolutionalLayerPanel;
 import org.neuroph.netbeans.wizards.ConvolutionalNetworkVisualPanel1;
+import org.neuroph.netbeans.wizards.ConvolutionalNetworkVisualPanel2;
+import org.neuroph.netbeans.wizards.PoolingLayerPanel;
 import org.neuroph.nnet.ConvolutionalNetwork;
 import org.neuroph.nnet.comp.Kernel;
 import org.neuroph.nnet.comp.layer.Layer2D;
@@ -14,88 +19,55 @@ public class ConvolutionalNetworkWizard implements NetworkWizard {
 
     @Override
     public Component getComponent() {
-        return ConvolutionalNetworkVisualPanel1.getInstance();
+        return ConvolutionalNetworkVisualPanel2.getInstance();
     }
 
     @Override
     public NeuralNetwork<? extends LearningRule> createNeuralNetwork(WizardDescriptor settings) {
         String neuralNetworkName = (String) settings.getProperty("neural network name");
-        String kernelWidth = (String) settings.getProperty("kernelWidth");
-        String kernelHeight = (String) settings.getProperty("kernelHeight");
+        String outputNeuronCount = (String) settings.getProperty("outputNeuronCount");
         String inputWidth = (String) settings.getProperty("inputWidth");
         String inputHeight = (String) settings.getProperty("inputHeight");
-        String outputNeuronCount = (String) settings.getProperty("outputNeuronCount");
-        String[] numberOfMaps = ((String) settings.getProperty("numberOfMaps")).split(" ");
-        
+        List<JPanel> panelList = (List<JPanel>) settings.getProperty("layers");
 
         Layer2D.Dimensions inputDimension = new Layer2D.Dimensions(Integer.parseInt(inputWidth), Integer.parseInt(inputHeight));
-        Kernel convolutionKernel = new Kernel(Integer.parseInt(kernelWidth), Integer.parseInt(kernelHeight));
-        Kernel poolingKernel = new Kernel(2, 2);
+        ConvolutionalNetwork convolutionNetwork;
 
-        ConvolutionalNetwork convolutionNetwork = new ConvolutionalNetwork.ConvolutionalNetworkBuilder(inputDimension, 1)
-                .withConvolutionLayer(convolutionKernel, Integer.parseInt(numberOfMaps[0]))
-                .withPoolingLayer(poolingKernel)
-                .withConvolutionLayer(convolutionKernel, Integer.parseInt(numberOfMaps[1]))
-                .withPoolingLayer(poolingKernel)
-                .withConvolutionLayer(convolutionKernel, Integer.parseInt(numberOfMaps[2]))
-                .withFullConnectedLayer(Integer.parseInt(outputNeuronCount))
-                .createNetwork();
+        ConvolutionalNetwork.ConvolutionalNetworkBuilder cnnBuilder = new ConvolutionalNetwork.ConvolutionalNetworkBuilder(inputDimension, 1);
 
+        for (JPanel panel : panelList) {
+            if (panel.getToolTipText().equals("clp")) {
+                ConvolutionalLayerPanel clp = (ConvolutionalLayerPanel) panel;
+                int nuberOfMaps = Integer.parseInt(clp.getJtxtNumberOfMaps().getText().trim());
+                Kernel k = new Kernel(Integer.parseInt(clp.getJtxtKernelWidth().getText().trim()), Integer.parseInt(clp.getJtxtKernelheight().getText().trim()));
+                cnnBuilder = cnnBuilder.withConvolutionLayer(k, nuberOfMaps);
+
+            } else {
+                PoolingLayerPanel clp = (PoolingLayerPanel) panel;
+                int nuberOfMaps = Integer.parseInt(clp.getJtxtNumberOfMaps().getText().trim());
+                Kernel k = new Kernel(Integer.parseInt(clp.getJtxtKernelWidth().getText().trim()), Integer.parseInt(clp.getJtxtKernelheight().getText().trim()));
+                cnnBuilder = cnnBuilder.withConvolutionLayer(k, nuberOfMaps);
+            }
+        }
+        int outputNeouron = Integer.parseInt(outputNeuronCount);
+        cnnBuilder = cnnBuilder.withFullConnectedLayer(outputNeouron);
+        convolutionNetwork = cnnBuilder.createNetwork();
         convolutionNetwork.setLabel(neuralNetworkName);
-        ConvolutionalNetworkVisualPanel1.getInstance().clearForm();
-        return convolutionNetwork; 
+        ConvolutionalNetworkVisualPanel2.getInstance().clearForm();
+        return convolutionNetwork;
     }
 
     @Override
     public void storePanelData(Object settings) {
-        ((WizardDescriptor) settings).putProperty("kernelWidth", ((ConvolutionalNetworkVisualPanel1) getComponent()).getKernelWidth().getText().trim());
-        ((WizardDescriptor) settings).putProperty("kernelHeight", ((ConvolutionalNetworkVisualPanel1) getComponent()).getKernelHeigh().getText().trim());
-        ((WizardDescriptor) settings).putProperty("inputWidth", ((ConvolutionalNetworkVisualPanel1) getComponent()).getInputWidth().getText().trim());
-        ((WizardDescriptor) settings).putProperty("inputHeight", ((ConvolutionalNetworkVisualPanel1) getComponent()).getInputHeight().getText().trim());
-        ((WizardDescriptor) settings).putProperty("numberOfMaps", ((ConvolutionalNetworkVisualPanel1) getComponent()).getNumberOfMaps().getText().trim());
-        ((WizardDescriptor) settings).putProperty("outputNeuronCount", ((ConvolutionalNetworkVisualPanel1) getComponent()).getOutputNeurons().getText().trim());
+        ((WizardDescriptor) settings).putProperty("outputNeuronCount", ((ConvolutionalNetworkVisualPanel2) getComponent()).getJtxtNumberOfOutputNeurons().getText().trim());
+        ((WizardDescriptor) settings).putProperty("inputHeight", ((ConvolutionalNetworkVisualPanel2) getComponent()).getInputHeight().getText().trim());
+        ((WizardDescriptor) settings).putProperty("inputWidth", ((ConvolutionalNetworkVisualPanel2) getComponent()).getInputWidth().getText().trim());
+        ((WizardDescriptor) settings).putProperty("layers", ((ConvolutionalNetworkVisualPanel2) getComponent()).getPanelList());
 
     }
 
     @Override
     public void validatePanelData() throws WizardValidationException {
-        int a=0;
-        try {
-            Integer.parseInt(((ConvolutionalNetworkVisualPanel1) getComponent()).getKernelWidth().getText().trim());
-        } catch (NumberFormatException ex) {
-            throw new WizardValidationException(null, "Kernel width must be integer value!", null);
-        }
-        try {
-            Integer.parseInt(((ConvolutionalNetworkVisualPanel1) getComponent()).getKernelHeigh().getText().trim());
-        } catch (NumberFormatException ex) {
-            throw new WizardValidationException(null, "Kernel height must be integer value!", null);
-        }
-
-        try {
-            Integer.parseInt(((ConvolutionalNetworkVisualPanel1) getComponent()).getInputWidth().getText().trim());
-        } catch (NumberFormatException ex) {
-            throw new WizardValidationException(null, "Input width must beinteger value!", null);
-        }
-        try {
-            Integer.parseInt(((ConvolutionalNetworkVisualPanel1) getComponent()).getInputHeight().getText().trim());
-        } catch (NumberFormatException ex) {
-            throw new WizardValidationException(null, "Input height must be  integer value!", null);
-        }
-        try {
-            Integer.parseInt(((ConvolutionalNetworkVisualPanel1) getComponent()).getOutputNeurons().getText().trim());
-        } catch (NumberFormatException ex) {
-            throw new WizardValidationException(null, "Output neuron count must be integer value!", null);
-        }
-        String[] numberOfMaps = ((ConvolutionalNetworkVisualPanel1) getComponent()).getNumberOfMaps().getText().trim().split(" ");
-
-        for (String number : numberOfMaps) {
-            try {
-                Integer.parseInt(number);
-            } catch (NumberFormatException ex) {
-                throw new WizardValidationException(null, "Number of maps must be integer value seperated with space!", null);
-            }
-        }
-
+        //
     }
-
 }
