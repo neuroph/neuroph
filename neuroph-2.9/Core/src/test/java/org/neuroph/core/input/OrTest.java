61eff56575806fcd1bce93cda971ead01686121a
vujicictijana <vujicic.tijana@gmail.com>
2017-05-31 17:01:10 +0200

    Tests for input functions

--- neuroph-2.9/Core/src/test/java/org/neuroph/core/input/OrTest.java
+++ neuroph-2.9/Core/src/test/java/org/neuroph/core/input/OrTest.java
@@ -1,132 +1,86 @@
 
-
 package org.neuroph.core.input;
 
-import java.util.ArrayList;
-import java.util.List;
 import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 import org.neuroph.core.Connection;
 import org.neuroph.core.Neuron;
 import org.neuroph.nnet.comp.neuron.InputNeuron;
-import static org.junit.Assert.assertEquals;
 
 /**
  *
- * @author Shivanth
+ * @author Shivanth, Tijana
  */
+@RunWith(value = Parameterized.class)
 public class OrTest {
-    private Or instance;
-    private List<Connection> inputConnections;
-    private List<InputNeuron> inputNeurons;    
+	Or instance;
+	Connection[] inputConnections;
+	InputNeuron[] inputNeurons;
+	double[] inputs;
+	double expected;
 
+	public OrTest(DoubleArray inputs, double expected) {
+		this.inputs = inputs.getArray();
+		this.expected = expected;
+	}
 
-    @BeforeClass
-    public static void setUpClass() throws Exception {
+	@Parameters
+	public static Collection<Object[]> getParamters() {
+		return Arrays.asList(new Object[][] { { new DoubleArray(new double[] { 0.49999d, 0d, 0d, 0d }), 0 },
+				{ new DoubleArray(new double[] { 0d, 0d, 0d, 0d }), 0 },
+				{ new DoubleArray(new double[] { 0.5d, 0d, 0d, 0d }), 1 },
+				{ new DoubleArray(new double[] { 1d, 0d, 0d, 0d }), 1 },
+				{ new DoubleArray(new double[] { 0.52d, 0.53d, 0.54d, 0.5d, 0.6d, 0.83d }), 1 },
+				{ new DoubleArray(new double[] { 0.32d, 0.44d, 0.33d, 0.5d, 0.4d, 0.23d }), 1 } });
 
-    }
+	}
 
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
+	@BeforeClass
+	public static void setUpClass() throws Exception {
 
-    @Before
-    public void setUp() {
-        instance = new Or();
-        
-        inputNeurons = new ArrayList<>(4);
-        for(int i=0; i<4; i++)
-            inputNeurons.add(new InputNeuron());
-        
-        Neuron toNeuron =  new Neuron();
-                
-        inputConnections = new ArrayList<Connection>(4);
-        for(int i=0; i<4; i++) {
-            Connection conn = new Connection(inputNeurons.get(i), toNeuron, 1);
-            inputConnections.add(conn) ;
-            toNeuron.addInputConnection(conn);
-        }          
-    }
+	}
 
-    @After
-    public void tearDown() {
-    }
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+	}
 
-   @Test
-    public void testZeroUpperBoundary() {
-        double[] inputs = new double[]{0.49999d, 0d, 0d, 0d};
-        for (int i = 0; i < inputNeurons.size(); i++) {
-            inputNeurons.get(i).setInput(inputs[i]);
-            inputNeurons.get(i).calculate();
-        }
+	@Before
+	public void setUp() {
+		instance = new Or();
 
-        double actualOutput = instance.getOutput(inputConnections);
-        assertEquals(0d, actualOutput, 0.000001d);
-    }
+		inputNeurons = new InputNeuron[4];
+		for (int i = 0; i < inputNeurons.length; i++)
+			inputNeurons[i] = new InputNeuron();
 
-	@Test
-    public void testZeroLowerBoundary() {
-        double[] inputs = new double[]{0d, 0d, 0d, 0d};
-        for (int i = 0; i < inputNeurons.size(); i++) {
-            inputNeurons.get(i).setInput(inputs[i]);
-            inputNeurons.get(i).calculate();
-        }
+		Neuron toNeuron = new Neuron();
 
-        double actualOutput = instance.getOutput(inputConnections);
-        assertEquals(0d, actualOutput, 0.000001d);
-    }
+		inputConnections = new Connection[4];
+		for (int i = 0; i < inputConnections.length; i++) {
+			inputConnections[i] = new Connection(inputNeurons[i], toNeuron, 1);
+			toNeuron.addInputConnection(inputConnections[i]);
+		}
+	}
+
+	@After
+	public void tearDown() {
+	}
 
 	@Test
-    public void testOneLowerBoundary() {
-        double[] inputs = new double[]{0.5d, 0d, 0d, 0d};
-        for (int i = 0; i < inputNeurons.size(); i++) {
-            inputNeurons.get(i).setInput(inputs[i]);
-            inputNeurons.get(i).calculate();
-        }
+	public void testGetOutput() {
+		for (int i = 0; i < inputNeurons.length; i++) {
+			inputNeurons[i].setInput(inputs[i]);
+			inputNeurons[i].calculate();
+		}
 
-        double actualOutput = instance.getOutput(inputConnections);
-        assertEquals(1d, actualOutput, 0.000001d);
-    }
+		double result = instance.getOutput(inputConnections);
+		assertEquals(expected, result, 0.000001);
+	}
 
-	@Test
-    public void testOneUpperBoundary() {
-        double[] inputs = new double[]{1d, 0d, 0d, 0d};
-        for (int i = 0; i < inputNeurons.size(); i++) {
-            inputNeurons.get(i).setInput(inputs[i]);
-            inputNeurons.get(i).calculate();
-        }
-
-        double actualOutput = instance.getOutput(inputConnections);
-        assertEquals(1d, actualOutput, 0.000001d);
-    }
-
-	@Test
-    public void testOneForManyInputs() {
-        double[] inputs = new double[]{0.52d, 0.53d, 0.54d, 0.5d, 0.6d, 0.83d};
-        for (int i = 0; i < inputNeurons.size(); i++) {
-            inputNeurons.get(i).setInput(inputs[i]);
-            inputNeurons.get(i).calculate();
-        }
-
-        double actualOutput = instance.getOutput(inputConnections);
-        assertEquals(1d, actualOutput, 0.000001d);
-    }
-
-    @Test
-    public void testZeroForManyInputs() {
-        double[] inputs = new double[]{0.32d, 0.44d, 0.33d, 0.5d, 0.4d, 0.23d};
-        for (int i = 0; i < inputNeurons.size(); i++) {
-            inputNeurons.get(i).setInput(inputs[i]);
-            inputNeurons.get(i).calculate();
-        }
-
-        double actualOutput = instance.getOutput(inputConnections);
-        assertEquals(1d, actualOutput, 0.000001d);
-    }
-
-    @Test
-    public void testNoInputs() {
-        double actualOutput = instance.getOutput(new ArrayList<Connection>());
-        assertEquals(0d, actualOutput, 0.000001d);
-    }
 }
