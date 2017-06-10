d0ea76268bd08eb23c06b77f733b825832137a3f
Fabien DUMINY <fduminy@jnode.org>
2017-06-10 19:01:20 +0200

    Fix broken test in class FeatureMapTest.

--- neuroph-2.9/Contrib/src/test/java/org/neuroph/contrib/FeatureMapTest.java
+++ neuroph-2.9/Contrib/src/test/java/org/neuroph/contrib/FeatureMapTest.java
@@ -1,6 +1,7 @@
 package org.neuroph.contrib;
 
-import org.junit.Assert;
+import org.assertj.core.api.JUnitSoftAssertions;
+import org.junit.Rule;
 import org.junit.Test;
 import org.neuroph.nnet.comp.Dimension2D;
 import org.neuroph.nnet.comp.layer.FeatureMapLayer;
@@ -8,14 +9,15 @@ import org.neuroph.nnet.comp.neuron.InputNeuron;
 import org.neuroph.util.NeuronProperties;
 
 public class FeatureMapTest {
+	@Rule
+	public JUnitSoftAssertions softly = new JUnitSoftAssertions();
 
 	@Test
 	public void testEmptyFeatureMap() {
 		Dimension2D dimension = new Dimension2D(0, 0);
 		FeatureMapLayer featureMap = new FeatureMapLayer(dimension, new NeuronProperties());
-		Assert.assertEquals(0, featureMap.getNeuronsCount());
-		Assert.assertEquals(0, featureMap.getHeight());
-		Assert.assertEquals(0, featureMap.getWidth());
+
+		assertFeatureMap(featureMap, dimension);
 	}
 
 	@Test
@@ -26,9 +28,7 @@ public class FeatureMapTest {
 		inputNeuron.setInput(1);
 		featureMap.addNeuron(inputNeuron);
 
-		Assert.assertEquals(1, featureMap.getNeuronsCount());
-		Assert.assertEquals(dimension.getWidth(), featureMap.getWidth());
-		Assert.assertEquals(dimension.getHeight(), featureMap.getHeight());
+		assertFeatureMap(featureMap, dimension, inputNeuron);
 	}
 
 	@Test
@@ -48,7 +48,13 @@ public class FeatureMapTest {
 		featureMap.addNeuron(inputNeuron3);
 		featureMap.addNeuron(inputNeuron4);
 
-		Assert.assertEquals(4, featureMap.getNeuronsCount());
+		assertFeatureMap(featureMap, dimension, inputNeuron1, inputNeuron2, inputNeuron3, inputNeuron4);
 	}
 
+	private void assertFeatureMap(FeatureMapLayer featureMap, Dimension2D dimension, InputNeuron... inputNeurons) {
+		softly.assertThat(featureMap.getNeuronsCount())
+		      .isEqualTo(dimension.getWidth() * dimension.getHeight() + inputNeurons.length);
+		softly.assertThat(featureMap.getWidth()).isEqualTo(dimension.getWidth());
+		softly.assertThat(featureMap.getHeight()).isEqualTo(dimension.getHeight());
+	}
 }
