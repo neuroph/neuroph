f3bde3b91df9cf884fb7822332542c8f13fba1ca
Zoran Sevarac <sevarac@gmail.com>
2019-03-28 11:32:45 +0100

    Neuroph Framework split commit

--- NeurophStudio/NeurophProjectFiles/src/org/neuroph/netbeans/files/nnet/package-info.java
+++
@@ -1,4 +0,0 @@
-@TemplateRegistration(folder = "Other", content = "NeuralNetworkTemplate.nnet")
-package org.neuroph.netbeans.files.nnet;
-
-import org.netbeans.api.templates.TemplateRegistration;
--- NeurophStudio/NeurophProjectFiles/src/org/neuroph/netbeans/files/tset/package-info.java
+++
@@ -1,4 +0,0 @@
-@TemplateRegistration(folder = "Other", content = "DataSetTemplate.tset")
-package org.neuroph.netbeans.files.tset;
-
-import org.netbeans.api.templates.TemplateRegistration;
